42590c79a7b8ae89b19ef6a8ca1eae0c20d79604
Satakun Utama <49134472+lines-of-codes@users.noreply.github.com>
2021-06-27 12:01:50 +0700

    Implement basic types

---
+++ llvmtypes.py
@@ -0,0 +1,23 @@
+from llvmlite import ir
+
+# Integers
+int_sbyte = ir.IntType(8) # Signed byte
+int_short = ir.IntType(16) # Short
+int_integer = ir.IntType(32) # Standard Integer
+int_long = ir.IntType(64) # Long
+
+# Type Size reference: C# Type byte size
+
+# Other numeric types
+float_float = ir.FloatType()
+double_double = ir.DoubleType()
+
+# Special types
+void_void = ir.VoidType() # Return type for methods
+
+# Arrays
+def InitializeArray(valtype, size):
+	return ir.ArrayType(valtype, size)
+
+def InitializeList(valtype, start_size):
+	return ir.VectorType(valtype, start_size)
